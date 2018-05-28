21549b7f05b0e9d82d37f95f511c4cf15a5ba028
stevannohero <stevannohero@gmail.com>
2018-05-28 15:10:36 +0700

    button and link

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component} from "react";
+import "./App.css";
+
+class App extends Component{
+  render(){
+    return(
+      <div className="App">
+      	<div className="Rectangle-10-Copy">
+        	<div className="Rectangle-6">
+        		<div className="CHECK-OUT">CHECK OUT</div>
+        	</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
